a91d2d7d1a6ef80b90ed89cbd0d3055bac5e09f1
Vladislav Simovic <vladislav.simovic@acronis.com>
2024-07-26 15:05:15 +0200

    Failed find and replace

--- FindAndReplace/ViewController.swift
+++ FindAndReplace/ViewController.swift
@@ -11,10 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let text = "Install Acronis Cyber Protect Home Office on your computer. For detailed instructions refer to the installation guide. Run Acronis Cyber Protect Home Office, create a new backup, and select Mobile as the backup source. "
         
-        let valueToPresent = "Install SHARINGAN ACRONIS on your computer. For detailed instructions refer to the installation guide. Run SHARINGAN ACRONIS, create a new backup, and select Mobile as the backup source. "
-        
-        let text = "Install SHARINGAN ACRONIS on your computer. For detailed instructions refer to the installation guide. Run SHARINGAN ACRONIS, create a new backup, and select Mobile as the backup source. "
+        print(text)
     }
 }
 
